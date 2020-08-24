0eeb0722c85ff3e4538195833f7b1a45abee225d
Sam Little <kagutaba256@gmail.com>
2020-08-23 18:10:49 -0700

    adding terrain stuff

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -9,6 +9,9 @@ public class MeshGenerator : MonoBehaviour {
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
+
     // Start is called before the first frame update
     void Start () {
         mesh = new Mesh ();
@@ -16,19 +19,33 @@ public class MeshGenerator : MonoBehaviour {
         CreateShape ();
         UpdateMesh ();
     }
-
     void CreateShape () {
-        vertices = new Vector3[] {
-            new Vector3 (0, 0, 0),
-            new Vector3 (0, 0, 1),
-            new Vector3 (1, 0, 0)
-        };
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        for (int i = 0, z = 0; z <= zSize; z++) {
+            for (int x = 0; x <= xSize; x++) {
+                float y = Mathf.PerlinNoise (x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3 (x, y, z);
+                i++;
+            }
+        }
 
-        triangles = new int[] {
-            0,
-            1,
-            2
-        };
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < zSize; z++) {
+            for (int x = 0; x < xSize; x++) {
+                triangles[0 + tris] = vert + 0;
+                triangles[1 + tris] = vert + xSize + 1;
+                triangles[2 + tris] = vert + 1;
+                triangles[3 + tris] = vert + 1;
+                triangles[4 + tris] = vert + xSize + 1;
+                triangles[5 + tris] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
     }
 
     void UpdateMesh () {
@@ -38,4 +55,11 @@ public class MeshGenerator : MonoBehaviour {
 
         mesh.RecalculateNormals ();
     }
+
+    // private void OnDrawGizmos () {
+    //     if (vertices == null) return;
+    //     for (int i = 0; i < vertices.Length; i++) {
+    //         Gizmos.DrawSphere (vertices[i], .1f);
+    //     }
+    // }
 }
